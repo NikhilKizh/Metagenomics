e2de904ecd55196f2b1a53ef6e63a2d41b0bea56
NikhilKizh <126772960+NikhilKizh@users.noreply.github.com>
2023-07-25 12:23:26 +0100

    Update s03a_faith-pd_visualization.py

--- Scripts/16S/s03a_faith-pd_visualization.py
+++ Scripts/16S/s03a_faith-pd_visualization.py
@@ -2,5 +2,5 @@
 
 qiime diversity alpha-group-significance \
   --i-alpha-diversity core-metrics-results/faith_pd_vector.qza \
-  --m-metadata-file 16Smetadata.tsv \
+  --m-metadata-file metadata16S.tsv \
   --o-visualization core-metrics-results/faith-pd-group-significance.qzv
