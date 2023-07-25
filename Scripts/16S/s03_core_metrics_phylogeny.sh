6552a12c48cbd314f3952079929869b507953aeb
NikhilKizh <126772960+NikhilKizh@users.noreply.github.com>
2023-07-25 12:22:34 +0100

    Update s03_core_metrics_phylogeny.sh

--- Scripts/16S/s03_core_metrics_phylogeny.sh
+++ Scripts/16S/s03_core_metrics_phylogeny.sh
@@ -5,5 +5,5 @@ qiime diversity core-metrics-phylogenetic \
   --i-phylogeny 16S_rooted-tree.qza \ 
   --i-table 16S_table_dada2.qza \
   --p-sampling-depth 44300 \
-  --m-metadata-file 16Smetadata.tsv \ 
+  --m-metadata-file metadata16S.tsv \ 
   --output-dir core-metrics-results
