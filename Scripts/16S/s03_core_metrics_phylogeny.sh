25b7bce974f3bf346f03e218c89a2206a2ee3a35
NikhilKizh <126772960+NikhilKizh@users.noreply.github.com>
2023-08-16 11:49:19 +0100

    Delete s03_core_metrics_phylogeny.sh

--- Scripts/16S/s03_core_metrics_phylogeny.sh
+++
@@ -1,9 +0,0 @@
-# core metrics calculation: Phylogeny
-# sampling depth 44300
-
-qiime diversity core-metrics-phylogenetic \
-  --i-phylogeny 16S_rooted-tree.qza \ 
-  --i-table 16S_table_dada2.qza \
-  --p-sampling-depth 44300 \
-  --m-metadata-file metadata16S.tsv \ 
-  --output-dir core-metrics-results
