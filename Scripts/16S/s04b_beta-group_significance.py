e6e0fef811ee1d3ea92289ffcb34bb15b2c0158a
NikhilKizh <126772960+NikhilKizh@users.noreply.github.com>
2023-08-16 11:50:04 +0100

    Delete s04b_beta-group_significance.py

--- Scripts/16S/s04b_beta-group_significance.py
+++
@@ -1,9 +0,0 @@
-# beta-group-significance_Day
-# metadata-column Day
-
-qiime diversity beta-group-significance \
-  --i-distance-matrix core-metrics-results/unweighted_unifrac_distance_matrix.qza \
-  --m-metadata-file metadata16S.tsv \
-  --m-metadata-column Day  \
-  --o-visualization core-metrics-results/beta-group-significance_Day.qzv \
-  --p-pairwise
