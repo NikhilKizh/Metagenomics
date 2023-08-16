aa098c533aa89b398dac741c147cb530890f4603
NikhilKizh <126772960+NikhilKizh@users.noreply.github.com>
2023-08-16 11:48:15 +0100

    Delete s01b_visualizing_feature_table.py

--- Scripts/16S/s01b_visualizing_feature_table.py
+++
@@ -1,6 +0,0 @@
-# visualizing feature table metadata
-
-qiime feature-table summarize \
-  --i-table 16S_table_dada2.qza \
-  --o-visualization 16S_table_dada2.qzv \
-  --m-sample-metadata-file 16Smetadata.tsv
