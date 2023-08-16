6d3b517b3e4831af5a5ce3eac8a8b16693360ced
NikhilKizh <126772960+NikhilKizh@users.noreply.github.com>
2023-08-16 11:49:29 +0100

    Delete s03b_evenness_visualization.py

--- Scripts/16S/s03b_evenness_visualization.py
+++
@@ -1,6 +0,0 @@
-# evenness visualization
-
-qiime diversity alpha-group-significance \
-  --i-alpha-diversity core-metrics-results/evenness_vector.qza \
-  --m-metadata-file metadata16S.tsv \
-  --o-visualization core-metrics-results/evenness-group-significance.qzv
