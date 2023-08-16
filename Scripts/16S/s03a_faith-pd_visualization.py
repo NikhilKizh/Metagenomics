12b2ad0df450227238a91ec7fdc8e1f728506dcb
NikhilKizh <126772960+NikhilKizh@users.noreply.github.com>
2023-08-16 11:49:44 +0100

    Delete s03a_faith-pd_visualization.py

--- Scripts/16S/s03a_faith-pd_visualization.py
+++
@@ -1,6 +0,0 @@
-# faith-pd visualisation
-
-qiime diversity alpha-group-significance \
-  --i-alpha-diversity core-metrics-results/faith_pd_vector.qza \
-  --m-metadata-file metadata16S.tsv \
-  --o-visualization core-metrics-results/faith-pd-group-significance.qzv
