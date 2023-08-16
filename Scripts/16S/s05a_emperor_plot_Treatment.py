85c1dfe17e0397b930f29337431b73abb650089c
NikhilKizh <126772960+NikhilKizh@users.noreply.github.com>
2023-08-16 11:50:22 +0100

    Delete s05a_emperor_plot_Treatment.py

--- Scripts/16S/s05a_emperor_plot_Treatment.py
+++
@@ -1,8 +0,0 @@
-# Emperor-plot_Treatment
-# custom-axes_Treatment
-
-qiime emperor plot \
-  --i-pcoa core-metrics-results/unweighted_unifrac_pcoa_results.qza \
-  --m-metadata-file metadata16S.tsv \
-  --p-custom-axes Treatment \
-  --o-visualization core-metrics-results/unweighted-unifrac-emperor-Treatment.qzv
