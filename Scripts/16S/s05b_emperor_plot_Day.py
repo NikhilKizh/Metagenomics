96df720290dd27fbdd44caf180ee2e5ccc88aead
NikhilKizh <126772960+NikhilKizh@users.noreply.github.com>
2023-08-16 11:50:35 +0100

    Delete s05b_emperor_plot_Day.py

--- Scripts/16S/s05b_emperor_plot_Day.py
+++
@@ -1,8 +0,0 @@
-# Emperor-plot_Day
-# custom-axes_Day
-
-qiime emperor plot \
-  --i-pcoa core-metrics-results/unweighted_unifrac_pcoa_results.qza \
-  --m-metadata-file metadata16S.tsv \
-  --p-custom-axes Day \
-  --o-visualization core-metrics-results/unweighted-unifrac-emperor-Day.qzv
