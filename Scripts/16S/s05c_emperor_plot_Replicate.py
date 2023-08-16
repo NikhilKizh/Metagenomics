93fdaead7c01ff6e02eaafed5fb0ba3909f9e9d7
NikhilKizh <126772960+NikhilKizh@users.noreply.github.com>
2023-08-16 11:50:46 +0100

    Delete s05c_emperor_plot_Replicate.py

--- Scripts/16S/s05c_emperor_plot_Replicate.py
+++
@@ -1,8 +0,0 @@
-# Emperor-plot_Replicate
-# custom-axes_Replicate
-
-qiime emperor plot \
-  --i-pcoa core-metrics-results/unweighted_unifrac_pcoa_results.qza \
-  --m-metadata-file metadata16S.tsv \
-  --p-custom-axes Replicate \
-  --o-visualization core-metrics-results/unweighted-unifrac-emperor-Replicate.qzv
